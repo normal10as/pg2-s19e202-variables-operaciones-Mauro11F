e731906c94065b1a71095e3ac1fc3c635e3b9e37
Mauro <Mauro@LAPTOP-IOFACOLC>
2019-05-20 02:48:47 -0300

    Dias__Ultimo de__Variables operacionales

---
+++ Dias/Program.vb
@@ -0,0 +1,19 @@
+Imports System
+
+
+'Crear un proyecto y un módulo llamado “días” 
+'utilizando una enumeración para los días 
+'de la semana en español, mostrar que día que 
+'corresponde al resultado
+
+Module Dias
+    Sub Main(args As String())
+
+        ' ----------TERMINAR----------
+
+        Console.WriteLine("Hello World!")
+
+
+
+    End Sub
+End Module
